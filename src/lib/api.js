b400a5a382db7eded0d01dee3ace6403db82ba29
kusal89 <kslnrnmyr@gmail.com>
2025-02-23 13:41:20 +0530

    base url amended

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || "https://fed-storefront-backend-kusalana.onrender.com";
+const BASE_URL = "https://fed-storefront-backend-kusalana.onrender.com";
 
 export const Api = createApi({
   reducerPath: "Api",
